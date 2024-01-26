1025e7f49c9ad3fff653813f0dca20616ddd5e90
Andrey Ageew <faceofdeviler@gmail.com>
2024-01-26 13:39:05 +0300

    Add data determination of rental periods

---
+++ src/config.py
@@ -0,0 +1,8 @@
+from datetime import datetime
+
+# Определение периодов проката
+periods = [
+    (datetime(2020, 1, 1), datetime(2020, 1, 7)),
+    (datetime(2020, 1, 15), datetime(2020, 2, 7))
+]
+
