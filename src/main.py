80ed2495e1c1987e373ba479aef4ab38d7ba4f48
Andrey Ageew <faceofdeviler@gmail.com>
2024-01-26 14:27:25 +0300

    The main script with the output of newspaper and TV scripts

--- src/main.py
+++ src/main.py
@@ -1,10 +1,30 @@
-from config import periods
-from services import expand_dates
-if __name__ == '__main__':
+from entities.city import City
+from entities.superhero import Superhero
+from entities.scenario import Scenario
+from media.newspaper import Newspaper
+from media.tv import TV
 
-    # Разжатие массива
-    expanded_dates = expand_dates(periods)
 
-    # Вывод результатов
-    for date in expanded_dates:
-        print(date)
+def main():
+    # Создание экземпляров классов
+    city_tokyo = City("Tokyo", "Godzilla")
+    city_kostroma = City("Kostroma", "Aliens")
+
+    superhero = Superhero("Chuck Norris", "PIU PIU")
+
+    media_newspaper = Newspaper()
+    media_tv = TV()
+
+    # Сценарий для Токио и газеты
+    scenario_tokyo_newspaper = Scenario(superhero, city_tokyo, media_newspaper)
+    scenario_tokyo_newspaper.play_out("stands near a skyscraper")
+
+    print("--------------------")
+
+    # Сценарий для Костромы и телевидения
+    scenario_kostroma_tv = Scenario(superhero, city_kostroma, media_tv)
+    scenario_kostroma_tv.play_out("abduct cows in the meadow")
+
+
+if __name__ == "__main__":
+    main()
